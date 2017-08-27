d845441e629ac3a49cd4caf7f631254ed76ad553
Ian Munsie <darkstarsword@gmail.com>
2017-08-27 21:49:49 +1000

    Dreamfall Chapters: Debugging
    This commit is mostly for informative purposes. It adds a bunch of
    debugging to pull and display values from the hud analysis shader live
    to track down why the HUD adjustment doesn't work on the ghosts before
    hitting F10.
    
    The results show:
    cb0 is fine
    vb0 is ok, though W goes to zero when the icon is exactly on the ghosts
    The code is executed
    find_quad_center() works fine
    calc_0bd32bb622c2d611_pos() is bad and returns 0
    
    This is still not entirely clear to my why this happens, but I have a
    solution...

--- ShaderFixes/hud_analyse.hlsl
+++ ShaderFixes/hud_analyse.hlsl
@@ -24,7 +24,7 @@ struct vb_0bd32bb622c2d611_entry
 StructuredBuffer<struct vb_0bd32bb622c2d611_entry> vb_0bd32bb622c2d611_struct : register(t100);
 
 // This comes from the mentioned shader to calculate o0
-float2 calc_0bd32bb622c2d611_pos(float4 pos)
+float4 calc_0bd32bb622c2d611_pos(float4 pos)
 {
 	float4 r0, o0;
 
@@ -33,7 +33,7 @@ float2 calc_0bd32bb622c2d611_pos(float4 pos)
 	r0 = cb0[2].xyzw * pos.zzzz + r0.xyzw;
 	o0 = cb0[3].xyzw * pos.wwww + r0.xyzw;
 
-	return o0.xy * float2(1, -1);
+	return o0 * float4(1, -1, 1, 1);
 }
 
 // Average the position of all vertices in this quad to find the center
@@ -52,14 +52,32 @@ float4 find_quad_center()
 	return pos / 4;
 }
 
+RWBuffer<float4> debug_buf;
 
 [numthreads(1, 1, 1)]
 void main()
 {
+	debug_buf[8] = 1;
+
 	if (HUD_Depth_UAV[0].selection_circle_seen)
 		return;
 
-	HUD_Depth_UAV[0].pos = calc_0bd32bb622c2d611_pos(find_quad_center());
+	debug_buf[0] = cb0[0];
+	debug_buf[1] = cb0[1];
+	debug_buf[2] = cb0[2];
+	debug_buf[3] = cb0[3];
+	debug_buf[4] = vb_0bd32bb622c2d611_struct[0].vb_position;
+	debug_buf[5] = vb_0bd32bb622c2d611_struct[1].vb_position;
+	debug_buf[6] = vb_0bd32bb622c2d611_struct[2].vb_position;
+	debug_buf[7] = vb_0bd32bb622c2d611_struct[3].vb_position;
+	debug_buf[9] = 1;
+	debug_buf[10] = find_quad_center();
+	//debug_buf[11] = float4(calc_0bd32bb622c2d611_pos(debug_buf[10]), 0, 0);
+	debug_buf[11] = calc_0bd32bb622c2d611_pos(debug_buf[10]);
+
+	HUD_Depth_UAV[0].pos = calc_0bd32bb622c2d611_pos(find_quad_center()).xy;
+	//HUD_Depth_UAV[0].pos = find_quad_center() / float2(1920, 1080) * 2;
+	//HUD_Depth_UAV[0].pos.y *= -1;
 
 	if (texture_filter == selection_circle)
 		HUD_Depth_UAV[0].selection_circle_seen = true;
